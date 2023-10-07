adf5fc6389769da91051fad4124878e2530d1ede
kadidia mah Dembele <kadidem1999@gmail.com>
2023-10-07 14:38:57 +0000

    mah_test

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:budgetflutter/User/InscriptionPage.dart';
 import 'package:flutter/material.dart';
 import 'User/InscriptionPage.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
- class MyApp extends StatelessWidget {
+//test
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-     Widget build(BuildContext context){
-  return MaterialApp(
-   title: 'Musaka',
-   debugShowCheckedModeBanner: false,
-   theme: ThemeData(
-      primarySwatch: Colors.blue,
-    useMaterial3: true,
-   ),
-   home: InscriptionPage(),
-  );}
- }
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Musaka',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: InscriptionPage(),
+    );
+  }
+}
