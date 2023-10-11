f8ce12e8a052312c8c3ce968fc79797474050bf1
Muhammad23 <md9624561@gmail.com>
2023-10-11 13:41:38 +0000

    New commit

--- lib/pages/budgetModel.dart
+++ lib/pages/budgetModel.dart
@@ -1,7 +1,9 @@
 class BudgetModel {
   final String categorie;
   final String montant;
+  final String montant_restant;
   final String date;
+  final String description;
 
-  BudgetModel({required this.categorie, required this.montant, required this.date});
+  BudgetModel({required this.categorie, required this.montant,required this.montant_restant, required this.date, required this.description,});
 }
